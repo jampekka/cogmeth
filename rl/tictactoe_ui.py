96bb1ef143d794f86cbb7f3b353b19c1d4c161a0
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2021-04-21 01:52:03 +0300

    RL sample

---
+++ rl/tictactoe_ui.py
@@ -0,0 +1,85 @@
+import tictactoe
+from browser import window, document
+import editor
+import json
+from collections import defaultdict
+from copy import deepcopy
+
+board_element = document.getElementById("board")
+status_element = document.getElementById("status")
+ai_move_element = document.getElementById("ai_move")
+
+board_values = json.load(open("values_10000.json", "r"))
+board_values = {repr(k): v for k, v in board_values}
+def update_board():
+    global board, turn
+    board_element.innerHTML = ''
+    result = tictactoe.get_result(board)
+    if result is None:
+        status_element.innerHTML = f"{turn} to move"
+    else:
+        status_element.innerHTML = result
+    for row_i, row in enumerate(board):
+        row_element = document.createElement("tr")
+        board_element.appendChild(row_element)
+        for col_i, cell in enumerate(row):
+            cell_element = document.createElement("td")
+            if cell != tictactoe.EMPTY:
+                cell_element.innerHTML = cell
+            elif result is None:
+                cell_element.bind("click", lambda *args, row=row_i, col=col_i: manual_move((row, col)))
+            
+            hypothesis = deepcopy(board)
+            hypothesis[row_i][col_i] = turn
+            try:
+                value = board_values[repr(hypothesis)]
+                color = window.d3.interpolatePiYG((value + 1)/2)
+                if turn == tictactoe.CROSS:
+                    cell_element.style.backgroundColor = color
+            except KeyError:
+                pass
+            row_element.appendChild(cell_element)
+    
+def manual_move(move):
+    global turn, board
+    board, result = tictactoe.play_move(board, turn, move)
+    turn = tictactoe.other_mark(turn)
+    update_board()
+
+def ai_move():
+    global turn, board
+    if tictactoe.get_result(board) is not None: return
+    code = editor.editor.getValue()
+    wtf = exec(code, tictactoe.__dict__, locals())
+    move = get_move(board, turn)
+    #board, result = tictactoe.play_turn(board, turn, tictactoe.least_bad_player)
+    board, result = tictactoe.play_move(board, turn, move)
+    turn = tictactoe.other_mark(turn)
+    update_board()
+ai_move_element.bind("click", lambda *args: ai_move())
+
+import inspect
+from algos_gen import algorithms
+default_algo = next(iter(algorithms.keys()))
+editor.editor.setValue(algorithms[default_algo])
+algos_el = document.getElementById("algos")
+for name, src in algorithms.items():
+    el = document.createElement("option")
+    el.innerHTML = name
+    algos_el.appendChild(el)
+
+def set_algo(ev):
+    editor.editor.setValue(algorithms[algos_el.value])
+algos_el.bind("change", set_algo)
+
+def new_game():
+    global turn, board
+    turn = tictactoe.CROSS
+    board = tictactoe.new_board()
+    update_board()
+document.getElementById("new_game").bind("click", lambda *args: new_game())
+
+
+
+new_game()
+#tictactoe.random_game()
---
+++ rl/ttt_rl.py
@@ -0,0 +1,95 @@
+from copy import deepcopy
+from collections import defaultdict
+import random
+
+import tictactoe
+
+def temporal_difference(values, prev_state, new_state, reward):
+    predicted_value = values[prev_state]
+    if new_state is not None:
+        observed_value = values[new_state] + reward
+    else:
+        observed_value = reward
+
+    prediction_error = observed_value - predicted_value
+    return prediction_error
+
+def update_values(values, prev_state, new_state, reward, learning_rate):
+    difference = temporal_difference(values, prev_state, new_state, reward)
+    update = learning_rate*difference
+    assert prev_state is not None
+    values[prev_state] += update
+    return values
+
+class TemporalDifferencePlayer:
+    def __init__(self, values, learning_rate=1.0, exploration_prob=0.0):
+        self.learning_rate = learning_rate
+        self.exploration_prob = exploration_prob
+        self.values = values
+        self.last_seen_state = None
+
+    def __call__(self, board, turn):
+        predicted_values = []
+        for move in tictactoe.empty_squares(board):
+            hypothesis = deepcopy(board)
+            row, col = move
+            hypothesis[row][col] = turn
+            hypothesis = tuple(map(tuple, hypothesis))
+            predicted_value = self.values[hypothesis]
+            predicted_values.append((predicted_value, hypothesis, move))
+        
+        #random.shuffle(predicted_values)
+        if random.random() > self.exploration_prob:
+            predicted_value, new_board, move = max(predicted_values, key=lambda x: x[0])
+        else:
+            predicted_value, new_board, move = random.choice(predicted_values)
+        
+        reward = 0
+        board_tuple = tuple(map(tuple, board))
+        if self.last_seen_state is not None:
+            self.values = update_values(self.values, self.last_seen_state,
+                    new_board, reward, self.learning_rate)
+
+        self.last_seen_state = new_board
+         
+        return move
+    
+    def game_over(self, player, result):
+        reward = tictactoe.outcome_score(player, result)
+        self.values = update_values(self.values, self.last_seen_state, None, reward, self.learning_rate)
+        return self.values
+
+def td_vs_random():
+    random.seed(1)
+    n_games = 1000
+    values = defaultdict(lambda: 0.0)
+    #td_player = TemporalDifferencePlayer()
+    random_player = tictactoe.random_player
+    results = []
+    for i in range(n_games):
+        crosser = TemporalDifferencePlayer(values, learning_rate=0.1, exploration_prob=0.1)
+        noughter = random_player
+        
+        result = tictactoe.play_game(crosser, noughter)
+        
+        reward = tictactoe.outcome_score(tictactoe.CROSS, result)
+        results.append(reward)
+        values = crosser.game_over(tictactoe.CROSS, result)
+    
+    board = tictactoe.new_board()
+    turn = tictactoe.CROSS
+    for move in tictactoe.empty_squares(board):
+            hypothesis = deepcopy(board)
+            row, col = move
+            hypothesis[row][col] = turn
+            hypothesis = tuple(map(tuple, hypothesis))
+            predicted_value = values[hypothesis]
+            print(predicted_value, hypothesis)
+    import json
+    with open(f"values_{n_games}.json", 'w') as f:
+        json.dump(list(values.items()), f)
+    print(sum(results)/len(results))
+    
+    
+if __name__ == "__main__":
+    td_vs_random()
